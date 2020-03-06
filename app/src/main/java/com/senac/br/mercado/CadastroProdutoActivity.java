e0621dc041bdc5c6f85a5ce2894c60749875a704
rodrigo.sgarcia3 <rodrigo@repaginando.com.br>
2020-03-05 21:59:59 -0300

    Commit Inicial

--- app/src/main/java/com/senac/br/mercado/CadastroProdutoActivity.java
+++ app/src/main/java/com/senac/br/mercado/CadastroProdutoActivity.java
@@ -2,7 +2,9 @@ package com.senac.br.mercado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroProdutoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CadastroProdutoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_produto);
+    }
+
+    public void telaInicial(View view){
+        Intent Cad = new Intent(this, MainActivity.class);
+        startActivity(Cad);
+
     }
 }
