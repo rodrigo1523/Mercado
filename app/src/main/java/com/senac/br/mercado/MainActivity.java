978d24c80c504438ba75f7bffbb901d15adc5dd2
rodrigo.sgarcia3 <rodrigo@repaginando.com.br>
2020-03-16 21:16:46 -0300

    Commit Inicial

--- app/src/main/java/com/senac/br/mercado/MainActivity.java
+++ app/src/main/java/com/senac/br/mercado/MainActivity.java
@@ -1,7 +1,9 @@
 package com.senac.br.mercado;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -45,14 +47,35 @@ public class MainActivity extends AppCompatActivity {
         listiView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                produto prod = (produto) parent.getItemAtPosition(position);
+                final produto prod = (produto) parent.getItemAtPosition(position);
 
-                produtoDAO dao = new produtoDAO(MainActivity.this);
-                dao.excluir(prod);
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    //adb.setView(Main.this);
+                    adb.setTitle("Certeza que Deseja Excluir");
+                    adb.setIcon(android.R.drawable.ic_dialog_alert);
+                    adb.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            produtoDAO dao = new produtoDAO(MainActivity.this);
+                            dao.excluir(prod);
 
-                Toast.makeText(MainActivity.this, "Item excluido com sucesso", Toast.LENGTH_SHORT).show();
-                Intent Cad = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(Cad);
+                            Toast.makeText(MainActivity.this, "Item excluido com sucesso", Toast.LENGTH_SHORT).show();
+                            Intent Cad = new Intent(MainActivity.this, MainActivity.class);
+                            startActivity(Cad);
+                            Toast.makeText(getApplicationContext(), "Sim", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    adb.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "Não", Toast.LENGTH_LONG).show();
+                            //finish();
+                        }
+                    });
+
+                    AlertDialog alertDialog = adb.create();
+                    alertDialog.show();
+
+
                 return true;
             }
         });
